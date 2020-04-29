fd464eaba18ef6afb0c76e50d838006bf1c9b03e
Igor Hevčuk <ihevcuk@omega-software.hr>
2020-04-29 14:13:15 +0200

    team members text

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -10,13 +10,12 @@ function Contact() {
             <div className="col-sm-5">
               <p>Contact us and we'll get back to you within 24 hours.</p>
               <p><span><i className="fas fa-map-marker-alt"></i> Zagreb, Croatia</span></p>
-              <p><span><i className="fas fa-phone"></i> +385 91 793 9153</span></p>
+              <p><span><i className="fas fa-phone"></i> +385 92 2616 472 | +385 91 793 9153</span></p>
               <p><span><i className="fas fa-envelope"></i> info@codifica.io</span></p>
             </div>
-            <div class="col-sm-7 pl-5 d-none d-lg-block">
+            <div class="col-sm-7 d-none d-lg-block">
               <div class="row">
-              <img className="img-fluid" width="512" alt="Europe-Croatia" 
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Europe-Croatia.svg/512px-Europe-Croatia.svg.png" />
+                <img style={{margin: 'auto'}} className="img-fluid" width="512" alt="Europe-Croatia" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Europe-Croatia.svg/512px-Europe-Croatia.svg.png" />
               </div>
             </div>
           </div>
--- src/sections/Team.js
+++ src/sections/Team.js
@@ -9,51 +9,51 @@ function Team() {
           <div class="row mt-5">
             <div class="col-sm-3">
               <div class="card" style={{width: "18rem"}}>
-                  <img src={''} class="card-img-top" alt="..."></img>
+                  {/* <img src={''} class="card-img-top" alt="..."></img> */}
                   <div class="card-body">
                   <h5 class="card-title">Igor Hevčuk</h5>
                   <p class="card-text">
-                    Codifica co-founder and senior software engineer. Igor likes to check what is new in technology world and then adopt it
-                    if they are suitable for day to day work. Igor is certified Java developer.
+                    Codifica co-founder and senior software engineer. Fullstack developer but more focused on backend development, machine learning and JVM based languages. 
+                    Igor is certified Java developer.
                   </p>
-                  <a href="https://www.linkedin.com/in/igorhevcuk/" target="_blank" class="btn btn-primary">LinkedIn profile</a>
+                  <a href="https://www.linkedin.com/in/igorhevcuk/" target="_blank" class="btn btn-primary"><i className="fab fa-linkedin-in"></i></a>
                 </div>
               </div>
             </div>
             <div class="col-sm-3">
               <div class="card" style={{width: "18rem"}}>
-                  <img src={''} class="card-img-top" alt="..."></img>
+                  {/* <img src={''} class="card-img-top" alt="..."></img> */}
                   <div class="card-body">
                   <h5 class="card-title">Nikola Babić</h5>
                   <p class="card-text">
-                    Codifica co-founder and senior software engineer. Nikola likes to check what is new in technology world and then adopt it
-                    if they are suitable for day to day work. Nikola is certified Java developer.
+                    Codifica co-founder and senior software engineer. Fullstack developer, but leans more towards backend development. Reactive programming evangelist. 
+                    Nikola is certified Java developer.
                   </p>
-                  <a href="https://www.linkedin.com/in/nikola-babi%C4%87-306837ab/" target="_blank" class="btn btn-primary">LinkedIn profile</a>
+                  <a href="https://www.linkedin.com/in/nikola-babi%C4%87-306837ab/" target="_blank" class="btn btn-primary"><i className="fab fa-linkedin-in"></i></a>
                 </div>
               </div>
             </div>
             <div class="col-sm-3">
               <div class="card" style={{width: "18rem"}}>
-                  <img src={''} class="card-img-top" alt="..."></img>
+                  {/* <img src={''} class="card-img-top" alt="..."></img> */}
                   <div class="card-body">
                   <h5 class="card-title">Fran Konecki</h5>
-                  <p class="card-text">Fran is our young collegue still focused on learning things with support of out senior developers.
-                    He is interested in machine learning and backend software development.
+                  <p class="card-text">
+                    Fran is fresh out of college, but still has few years of experience in backend software development. Learns things fast and shows interest in computer
+                    science fields.
                   </p>
-                  <a href="https://www.linkedin.com/in/fran-konecki-5445861a8/" target="_blank" class="btn btn-primary">LinkedIn profile</a>
+                  <a href="https://www.linkedin.com/in/fran-konecki-5445861a8/" target="_blank" class="btn btn-primary"><i className="fab fa-linkedin-in"></i></a>
                 </div>
               </div>
             </div>
             <div class="col-sm-3">
               <div class="card" style={{width: "18rem"}}>
-                  <img src={''} class="card-img-top" alt="..."></img>
+                  {/* <img src={''} class="card-img-top" alt="..."></img> */}
                   <div class="card-body">
                   <h5 class="card-title">Mirza Mešić</h5>
-                  <p class="card-text">Like Fran, Mirza is also still focused on learning things. He is mix of unmixable - 
-                    programming software for embeddable devices and making frontend for out business clients.
-                  </p>
-                  <a href="https://www.linkedin.com/in/mirza-me%C5%A1i%C4%87/" target="_blank" class="btn btn-primary">LinkedIn profile</a>
+                  <p class="card-text">Mirza is fresh out of college. Interested in few tech fields, like embedded software development and
+                    frontend development. In Codifica he is focused on frontend development.</p>
+                  <a href="https://www.linkedin.com/in/mirza-me%C5%A1i%C4%87/" target="_blank" class="btn btn-primary"><i className="fab fa-linkedin-in"></i></a>
                 </div>
               </div>
             </div>
