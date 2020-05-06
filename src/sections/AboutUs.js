5c813eedffd5b700f8a5fd5da3f3e079366107e3
Nikola Babić <nikola.babic@omega-software.hr>
2020-05-06 13:53:53 +0200

    Added more info about CareSpeak client and changed logo.

--- src/sections/AboutUs.js
+++ src/sections/AboutUs.js
@@ -8,10 +8,7 @@ function AboutUs() {
       <h4>We are a fresh software agency primarily focused on building enterprise grade software solutions.
           Early adopters of all things reactive. We are big advocates for building robust and high-throughput solutions 
           using reactive streams, reactive persistence drivers, and big data technologies.</h4>
-      <p>We are focused on opensource technologies and have working experience with lenghty number of technologies that can provide 
-        reliable, distributed and high availability applications. We can integrate in your existing team or we can work on project 
-        from beggining to the end on our own - we already were in both situations and it worked well. In our team we have a mix of
-        senior and junior developers and we have already beat a lot of challanges.
+      <p>We are mostly focused on open source technologies and have experience with lengthy number of diverse technologies that provide reliable, distributed, and high-availability applications. We can integrate into your existing team or manage the whole project from start to finish - we have successful track record in both scenarios. Our team consists of high performing and experienced professionals with various seniority levels.
       </p>
     </div>
     <div className="col-sm-6 center d-none d-lg-block">
